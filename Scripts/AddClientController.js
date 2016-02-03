f9c852aa8c8cd326b5238faecaee70ab2f4e5cce
oussama lachiheb <oussama.lachiheb@gmail.com>
2016-02-03 20:28:55 +0100

    Validation message fixed with angular ng-show

--- Scripts/AddClientController.js
+++ Scripts/AddClientController.js
@@ -1,26 +1,24 @@
 towingApp.registerCrtl('AddClientController', function($scope, $http) {
-       $scope.client = {};
-     // calling our submit function.
-       $scope.submitForm = function () {
-           console.log($scope.client)
-           // Posting data to our backend
-           //var client = new Object();
-           //client.cin = '124578';
-       $http({
-         method : 'POST',
-         url: 'http://localhost:4465/api/client',
-         data: $.param($scope.client),//forms user object
-         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-        })
-         .success(function(data, status, headers, config) {
-           if (data.errors) {
-             // Showing errors.
-             $scope.errorName = data.errors.name;
-             $scope.errorUserName = data.errors.username;
-             $scope.errorEmail = data.errors.email;
-           } else {
-             $scope.message = data.message;
-           }
-         });
-       };
-     });
+    $scope.client = {};
+    // calling our submit function.
+    $scope.submitForm = function() {
+        console.log($scope.client)
+        $http({
+                method: 'POST',
+                url: 'http://localhost:4465/api/client',
+                data: $.param($scope.client), //forms user object
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            })
+            .success(function(data, status, headers, config) {
+                if (data.errors) {
+                    // Showing errors.
+                    $scope.notok='il ya un erreur';
+                } else {
+                    $scope.message='Enregistrement effectué avec succées';
+                    $scope.client={};
+                }
+            });
+    };
+});
